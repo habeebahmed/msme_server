10b4a87d7cc93a1e685e068bdc24bb28520a976a
habeebahmed <habeebahmed900@gmail.com>
2019-03-06 16:08:18 +0530

    initial

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const express = require('express');
-const router = express.Router();
-
-//All routes.
-router.get('/', (req, res) => { res.send('Server running') });
-
-//Export the Router.
-module.exports = router;
